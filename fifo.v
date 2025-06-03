4102fbb676c9e2df2e766d0131eea98f3bf74223
Maria Camila <mcamilabfh@gmail.com>
2025-06-03 16:54:25 -0300

    L20 modulo fifo e fsm prontos

--- fifo.v
+++ fifo.v
@@ -1,21 +1,43 @@
 module fifo(
     input   clk,
-    input   rst_n,
+    input   rst_n, //Ativo em zero
 
-    // Write interface
-    input   wr_en,
-    input   [7:0] data_in,
-    output  full,
+    input   wr_en, //habilita a escrita
+    input   [7:0] data_in, // dado a ser escrito na FIFO
+    output  full, // diz se a FIFO está cheia
 
-    // Read interface
-    input   rd_en,
-    output  reg [7:0] data_out,
-    output  empty,
+    input   rd_en, //habilita a leitur
+    output  reg [7:0] data_out, //dado lido da FIFO
+    output  empty, //indica se a FIFO está vazia
 
-    // status
-    output reg [3:0] fifo_words  // Current number of elements
+    
+    output reg [3:0] fifo_words  //mostra quantos elementos tem dentro da FIFO (de 0 a 8).
 );
 
-//insira seu código aqui
+reg [2:0] write_ptr; // onde o próximo dado será escrito (0 a 7)
+reg [2:0] read_ptr; // onde o próximo dado será lido (0 a 7)
+reg [7:0] mem [0:7]; //memória da FIFO
+
+always @(posedge clk) begin
+    if (!rst_n) begin
+        write_ptr = 0;
+        read_ptr = 0;
+        fifo_words = 0;
+
+    end else if (wr_en && !full) begin
+        mem[write_ptr] <= data_in; //Escreve data_in na posição apontada por write_ptr
+        write_ptr = write_ptr + 1; //Avança o ponteiro
+        fifo_words = fifo_words + 1; //Incrementa o contador
+
+    end 
+    if (rd_en && !empty) begin
+        data_out <= mem[read_ptr]; //Lê da posição read_ptr e joga pra data_out
+        read_ptr = read_ptr +1; //Avança read_ptr
+        fifo_words = fifo_words -1; //Decrementa fifo_words
+    end
+end
+
+assign full = (fifo_words == 8); //full será 1 quando fifo_words for 8
+assign empty = (fifo_words == 0); //empty será 1 quando fifo_words for 0
 
 endmodule
--- fsm.v
+++ fsm.v
@@ -1,14 +1,41 @@
 module fsm(
     input   clk,
     input   rst_n,
+    input [3:0] fifo_words,
 
     output reg wr_en,
-
-    output [7:0] fifo_data,
+    output reg [7:0] fifo_data
     
-    input [3:0] fifo_words
+
 );
 
-// insira seu código aqui
+localparam  [1:0] 
+    WRITE = 2'b00,
+    WAIT = 2'b01;
+
+    reg [1:0] estado;
+
+always @(posedge clk) begin
+    if(!rst_n) begin 
+        estado <=WRITE;
+        fifo_data <= 8'hAA;
+        wr_en <= 1;
+    end else if((estado == WRITE) && (fifo_words == 5)) begin
+        estado <= WAIT;
+        wr_en = 1;
+        fifo_data <= 8'hAA;
+    end else if((estado == WAIT) && (fifo_words <=2)) begin
+        estado <= WRITE;
+        wr_en = 1;
+        fifo_data <= 8'hAA;
+    end else if(estado == WRITE) begin
+        wr_en = 1;
+        fifo_data <= 8'hAA;
+    end else begin
+        wr_en <= 0;
+        fifo_data <= 8'h00;
+    end
+end
+
 
 endmodule
